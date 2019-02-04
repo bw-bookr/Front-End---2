1b7fdf84875b14fc3cc921747c80991fff9b0452
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-02-04 17:58:38 -0600

    commit before stand up

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Book from './components/Book';
+
 class App extends Component {
-  
+
   render() {
     return (
       <div className="App">
       <h1>BOOK LIST</h1>
+      <Book />
         
       </div>
     );
