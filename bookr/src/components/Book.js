1920ff068fde8fbfc4f42d571aae85d6f8351934
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-02-06 18:08:56 -0600

    commit

--- bookr/src/components/Book.js
+++ bookr/src/components/Book.js
@@ -1,3 +1,43 @@
+// import React from 'react';
+// import { Card, CardImg, CardText, CardBody,
+//   CardTitle, CardSubtitle, Button } from 'reactstrap';
+  
+// import styled from "styled-components";
+
+// import { Link, Route, withRouter} from 'react-router-dom';
+
+// const Book = (props) => {
+//     console.log(props.id);
+//   return (
+//     <Link to={`/bookabout/${props.id}`}>
+//     <div
+//     onClick={(event) => console.log(props.id, event)}
+//     className="Book">
+//       <Card>
+//         <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+//         <CardBody>
+//           <CardTitle>{props.title}</CardTitle>
+//           <CardSubtitle>By: {props.author}</CardSubtitle>
+//           <CardText>{props.short_description}</CardText>
+//           <Button>Click for More</Button>
+//         </CardBody>
+//       </Card>
+//     </div>
+//     </Link>
+//   );
+// };
+
+// Book.defaultProps = {
+//     title: '',
+//     author: '',
+//     id: '' 
+// };
+
+// export default Book;
+
+
+
+
 import React from 'react';
 import { Link, Route, withRouter} from 'react-router-dom';
 
